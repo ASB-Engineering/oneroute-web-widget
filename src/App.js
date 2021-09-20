c3a3eaa5fd96663431a53fb6996dfdd3ea4cea40
Chukwuemeka Azonobi <emekamykael45@gmail.com>
2021-09-20 12:51:59 +0100

    fix: stling of signup form

--- src/App.js
+++ src/App.js
@@ -147,9 +147,9 @@ function App(props) {
     }
 
     // Convert to RGB value
-    var r = parseInt(color.substr(0, 2), 16);
-    var g = parseInt(color.substr(2, 2), 16);
-    var b = parseInt(color.substr(4, 2), 16);
+    var r = parseInt(color?.substr(0, 2), 16);
+    var g = parseInt(color?.substr(2, 2), 16);
+    var b = parseInt(color?.substr(4, 2), 16);
 
     // Get YIQ ratio
     var yiq = (r * 299 + g * 587 + b * 114) / 1000;
