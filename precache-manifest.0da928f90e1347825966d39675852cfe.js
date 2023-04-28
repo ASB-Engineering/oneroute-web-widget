ac4c687553fa468e1139fcecb142c249c1c3a32b
clandestine33 <ed.oguamanam@gmail.com>
2023-04-28 16:41:12 +0100

    Updates

---
+++ precache-manifest.0da928f90e1347825966d39675852cfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cabff93042c444703db7fbcc629c1a1",
+    "url": "/oneroute-web-widget/index.html"
+  },
+  {
+    "revision": "80fc167426d63decba23",
+    "url": "/oneroute-web-widget/static/css/main.c44ee42f.chunk.css"
+  },
+  {
+    "revision": "6eccfbb0b9b3b0398c7b",
+    "url": "/oneroute-web-widget/static/js/2.98e3ecd1.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/oneroute-web-widget/static/js/2.98e3ecd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fc167426d63decba23",
+    "url": "/oneroute-web-widget/static/js/main.e91cb04f.chunk.js"
+  },
+  {
+    "revision": "9c54b4596d94c9cc7923",
+    "url": "/oneroute-web-widget/static/js/runtime-main.1cb27685.js"
+  }
+]);
