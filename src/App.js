af8b424fde3a6189c3efacc317b57de2980517f4
emekamykael45 <emekamykael45@gmail.com>
2023-08-09 11:35:17 +0100

    fix: debugging 2

--- src/App.js
+++ src/App.js
@@ -88,11 +88,20 @@ function App(props) {
   // WebSocket.io Script Starts Here
   useEffect(() => {
     socket.on("newMessage", (data) => {
-      console.log(".");
+      console.log(
+        "Is convo mine? ",
+        data?.conversation?.id === localStorage.getItem("conversationId")
+      );
+      console.log(
+        "Is sender authed? ",
+        data?.message?.sender?.authUser === true
+      );
+      console.log(data, "sock... data");
       if (
         data?.conversation?.id === localStorage.getItem("conversationId") &&
         data?.message?.sender?.authUser === true
       ) {
+        console.log("Fetch triggered...");
         getConvoMessages(false, true);
       }
     });
