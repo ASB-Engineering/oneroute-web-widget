159ceca3b285509c3b608f7f345ec3090af84838
emekamykael45 <emekamykael45@gmail.com>
2023-09-12 14:45:17 +0100

    fix: updated base URL

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import "regenerator-runtime/runtime";
 
 import "./App.css";
 
-const baseURL = "https://api.staging.oneroute.io/";
-// const baseURL = "https://api.oneroute.io/";
+// const baseURL = "https://api.staging.oneroute.io/";
+const baseURL = "https://api.oneroute.io/";
 
 var socket = io(baseURL, {
   reconnectionDelay: 1000,
